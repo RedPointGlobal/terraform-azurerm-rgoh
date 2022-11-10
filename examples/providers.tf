8d57e73b61ba23b43a45e0f01fd7430bdade22cf
Jim Musana <42842390+JMusana1@users.noreply.github.com>
2022-11-10 01:44:41 -0500

    Update providers.tf

--- examples/providers.tf
+++ examples/providers.tf
@@ -3,7 +3,6 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = "3.20.0"
-      configuration_aliases = [azurerm.hub, azurerm.shared-platform, azurerm.connectivity]
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -11,47 +10,12 @@ terraform {
     }
   }
 
-  cloud {
-    organization = "redpointglobal"
-
-    workspaces {
-      tags = ["azure", "fish", "prod", "network"]
-    }
-  }
-}
 
 provider "azurerm" {
   client_id       = local.client_id
   client_secret   = var.client_secret
   tenant_id       = local.tenant_id
   subscription_id = local.rgoh_spoke_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "hub"
-  client_id       = local.client_id
-  client_secret   = var.client_secret
-  tenant_id       = local.tenant_id
-  subscription_id = local.rgoh_hub_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "shared-platform"
-  client_id       = local.client_id
-  client_secret   = var.client_secret
-  tenant_id       = local.tenant_id
-  subscription_id = local.rgoh_shared_platform_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "connectivity"
-  client_id       = local.client_id
-  client_secret   = var.client_secret
-  tenant_id       = local.tenant_id
-  subscription_id = local.rgoh_hub_subscription_id
   features {}
 }
 
