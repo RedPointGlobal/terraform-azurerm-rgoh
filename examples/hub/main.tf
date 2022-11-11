b1a8b58d9860753f6afb76cab0cfe0b49756b387
jim.musana <jim.musana@redpointglobal.com>
2022-11-11 00:29:20 +0000

    commit on main

--- examples/hub/main.tf
+++ examples/hub/main.tf
@@ -1,9 +1,14 @@
 locals {
 
-  rgoh_hub_region              = "eastus2"
-  rgoh_hub_resource_group_name = "RGOH-hubeastus2"
-  environment                  = "hub"
-  subscription_id              = "aa47f583-fdf8-4ed6-acf8-46fcd7015701"
+  rgoh_hub_region                  = "eastus2"
+  rgoh_hub_resource_group_name     = "RGOH-hubeastus2"
+  environment                      = "hub"
+  subscription_id                  = "aa47f583-fdf8-4ed6-acf8-46fcd7015701"
+  virtual_network_address_space    = ["10.175.8.0/23"]
+  virtual_network_name             = "vnet-hub-eastus2"
+  virtual_network_dns_servers      = ["10.153.12.68"] # Internal IP address of Azure Firewall
+  firewall_subnet_address_prefixes = ["10.175.8.0/26"]
+  gateway_subnet_address_prefix    = ["10.175.9.0/26"]
 }
 
 module "rgoh_hub" {
@@ -17,9 +22,9 @@ module "rgoh_hub" {
   create_virtual_network_gateway = true
 
   // VIRTUAL NETWORK
-  virtual_network_address_space = ["10.175.8.0/23"]
-  virtual_network_name          = "vnet-hub-eastus2"
-  virtual_network_dns_servers   = ["10.153.12.68"]
+  virtual_network_address_space = local.virtual_network_address_space
+  virtual_network_name          = local.virtual_network_name
+  virtual_network_dns_servers   = local.virtual_network_dns_servers
 
   tags = {
     provisioner = "terraform"
@@ -30,14 +35,14 @@ module "rgoh_hub" {
 
     "firewall" = {
       name                                          = "AzureFirewallSubnet"
-      address_prefixes                              = ["10.175.8.0/26"]
+      address_prefixes                              = local.firewall_subnet_address_prefixes
       private_endpoint_network_policies_enabled     = true
       private_link_service_network_policies_enabled = true
     }
 
     "vpn" = {
       name                                          = "GatewaySubnet"
-      address_prefixes                              = ["10.175.9.0/26"]
+      address_prefixes                              = local.gateway_subnet_address_prefix
       private_endpoint_network_policies_enabled     = true
       private_link_service_network_policies_enabled = true
     }
