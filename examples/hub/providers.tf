5cf25338eafb361d6c554eb416c35f6514c2c6c7
jim.musana <jim.musana@redpointglobal.com>
2022-11-14 23:44:50 +0000

    commit on main

--- examples/hub/providers.tf
+++ examples/hub/providers.tf
@@ -14,7 +14,7 @@ terraform {
 
 provider "azurerm" {
   client_id       = var.client_id
-  client_secret   = var.client_id
+  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   features {}
