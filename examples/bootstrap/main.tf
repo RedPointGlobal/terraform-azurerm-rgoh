ab88673299a21b9c660f0aa332b987ff366575cc
jim.musana <jim.musana@redpointglobal.com>
2022-11-15 04:08:37 +0000

    commit on main

--- examples/bootstrap/main.tf
+++ examples/bootstrap/main.tf
@@ -1,38 +1,39 @@
 data "tfe_organizations" "main" {
+  name = "black-devops"
 }
 
 resource "tfe_workspace" "main" {
-  name         = "argo-sandbox"
+  name         = "tfc-rgoh-onboard"
   organization = data.tfe_organizations.main.id
   tag_names    = ["argo", "sandbox", "jmusana"]
 }
 
 resource "tfe_variable" "client_id" {
   key          = "client_id"
-  value         = "7838e8f3-dba4-48de-82cd-b1d065f1efd4"
-  category      = "terraform"
+  value        = "7838e8f3-dba4-48de-82cd-b1d065f1efd4"
+  category     = "terraform"
   workspace_id = tfe_workspace.main.id
 }
 
 resource "tfe_variable" "client_secret" {
   key          = "client_secret"
-  value         = var.client_secret
-  category      = "terraform"
-  sensitive     = true
+  value        = var.client_secret
+  category     = "terraform"
+  sensitive    = true
   workspace_id = tfe_workspace.main.id
 
 }
 
 resource "tfe_variable" "tenant_id" {
   key          = "tenant_id"
-  value         = "16a3d264-4987-408a-a6aa-69dd136253fc"
-  category      = "terraform"
+  value        = "16a3d264-4987-408a-a6aa-69dd136253fc"
+  category     = "terraform"
   workspace_id = tfe_workspace.main.id
 }
 
 resource "tfe_variable" "subscription_id" {
   key          = "subscription_id"
-  value         = var.subscription_id
-  category      = "terraform"
+  value        = var.subscription_id
+  category     = "terraform"
   workspace_id = tfe_workspace.main.id
 }
