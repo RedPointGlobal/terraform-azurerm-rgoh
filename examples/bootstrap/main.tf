c3854454e6b692a2248fe39ff386e5adcd6148c4
jim.musana <jim.musana@redpointglobal.com>
2022-11-15 05:09:22 +0000

    commit on main

--- examples/bootstrap/main.tf
+++ examples/bootstrap/main.tf
@@ -1,10 +1,16 @@
-data "tfe_organizations" "main" {
+resource "tfe_organization" "main" {
+  name  = "tfc-black-devops"
+  email = "musanajim@gmail.com"
 }
 
 resource "tfe_workspace" "main" {
   name         = "tfc-rgoh-onboard"
-  organization = data.tfe_organizations.main.id
+  organization = tfe_organization.main.id
   tag_names    = ["argo", "sandbox", "jmusana"]
+
+  depends_on = [
+    tfe_organization.main
+  ]
 }
 
 resource "tfe_variable" "client_id" {
@@ -12,6 +18,10 @@ resource "tfe_variable" "client_id" {
   value        = "7838e8f3-dba4-48de-82cd-b1d065f1efd4"
   category     = "terraform"
   workspace_id = tfe_workspace.main.id
+
+  depends_on = [
+    tfe_workspace.main
+  ]
 }
 
 resource "tfe_variable" "client_secret" {
@@ -21,6 +31,10 @@ resource "tfe_variable" "client_secret" {
   sensitive    = true
   workspace_id = tfe_workspace.main.id
 
+  depends_on = [
+    tfe_workspace.main
+  ]
+
 }
 
 resource "tfe_variable" "tenant_id" {
@@ -28,6 +42,10 @@ resource "tfe_variable" "tenant_id" {
   value        = "16a3d264-4987-408a-a6aa-69dd136253fc"
   category     = "terraform"
   workspace_id = tfe_workspace.main.id
+
+  depends_on = [
+    tfe_workspace.main
+  ]
 }
 
 resource "tfe_variable" "subscription_id" {
@@ -35,4 +53,8 @@ resource "tfe_variable" "subscription_id" {
   value        = var.subscription_id
   category     = "terraform"
   workspace_id = tfe_workspace.main.id
+
+  depends_on = [
+    tfe_workspace.main
+  ]
 }
