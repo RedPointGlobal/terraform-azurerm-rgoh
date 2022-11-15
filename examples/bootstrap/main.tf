db75103b3d99f76a2d1555b66d4b1d7a32c9adb7
jim.musana <jim.musana@redpointglobal.com>
2022-11-15 04:01:17 +0000

    commit on main

--- examples/bootstrap/main.tf
+++ examples/bootstrap/main.tf
@@ -7,16 +7,15 @@ resource "tfe_workspace" "main" {
   tag_names    = ["argo", "sandbox", "jmusana"]
 }
 
-
 resource "tfe_variable" "client_id" {
-  name          = "client_id"
+  key          = "client_id"
   value         = "7838e8f3-dba4-48de-82cd-b1d065f1efd4"
   category      = "terraform"
   workspace_id = tfe_workspace.main.id
 }
 
 resource "tfe_variable" "client_secret" {
-  name          = "client_secret"
+  key          = "client_secret"
   value         = var.client_secret
   category      = "terraform"
   sensitive     = true
@@ -25,14 +24,14 @@ resource "tfe_variable" "client_secret" {
 }
 
 resource "tfe_variable" "tenant_id" {
-  name          = "tenant_id"
+  key          = "tenant_id"
   value         = "16a3d264-4987-408a-a6aa-69dd136253fc"
   category      = "terraform"
   workspace_id = tfe_workspace.main.id
 }
 
 resource "tfe_variable" "subscription_id" {
-  name          = "subscription_id"
+  key          = "subscription_id"
   value         = var.subscription_id
   category      = "terraform"
   workspace_id = tfe_workspace.main.id
