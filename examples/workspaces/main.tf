45ad24576aaf6397b5784f125b06e6cf0edbf593
jim.musana <jim.musana@redpointglobal.com>
2022-11-16 04:59:22 +0000

    commit on main

--- examples/workspaces/main.tf
+++ examples/workspaces/main.tf
@@ -7,21 +7,17 @@ resource "tfe_variable" "client_id" {
   key          = "client_id"
   value        = "7838e8f3-dba4-48de-82cd-b1d065f1efd4"
   category     = "terraform"
-  workspace_id = tfe_workspace.main.id
+  workspace_id = tfe_workspace.client.id
 
-  depends_on = [
-    tfe_workspace.main
-  ]
+  depends_on = [tfe_workspace.main]
 }
 
 resource "tfe_variable" "tenant_id" {
   key          = "tenant_id"
   value        = "16a3d264-4987-408a-a6aa-69dd136253fc"
   category     = "terraform"
-  workspace_id = tfe_workspace.main.id
+  workspace_id = tfe_workspace.client.id
 
-  depends_on = [
-    tfe_workspace.main
-  ]
+  depends_on = [tfe_workspace.main]
 }
 
